611812497a9738c3ba8fc3644eae35956e7a0afe
PIRIS Marius <marius.piris@gmail.com>
2024-01-27 18:24:46 +0100

    Restart

--- SuperSushiSpeed/scripts/Death.gd
+++ SuperSushiSpeed/scripts/Death.gd
@@ -15,7 +15,7 @@ func _process(delta):
 
 
 func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/count_down.tscn")
 
 
 func _on_back_pressed():

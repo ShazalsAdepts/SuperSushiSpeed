a82b9f09acf82978a73ec21d6c2aecad17aff189
AnDiNoX <56691495+andinox@users.noreply.github.com>
2024-01-27 18:08:54 +0100

    score

--- SuperSushiSpeed/scripts/Menu.gd
+++ SuperSushiSpeed/scripts/Menu.gd
@@ -6,33 +6,39 @@ var connectCurrent = HTTPRequest.new()
 
 
 func _ready():
-	var de = FileAccess.open("res://usersave/user_login.json", FileAccess.READ)
-	var data = JSON.parse_string(de.get_as_text())
-	de.close()
-	var popup = get_node("../Panel")
-	var pseudo = get_node("Pseudo")
-	if data["player_identifier"] == null:
-		popup.set_visible(true)
+	if FileAccess.file_exists("res://usersave/user_login.json"):
+		var de = FileAccess.open("res://usersave/user_login.json", FileAccess.READ)
+		var data = JSON.parse_string(de.get_as_text())
+		de.close()
+		var popup = get_node("../Panel")
+		var pseudo = get_node("Pseudo")
+		if data["player_identifier"] == null:
+			popup.set_visible(true)
+		else:
+			popup.set_visible(false)
+			pseudo.set_visible(true)
+			player_identifier = data["player_identifier"]
+			var hearders = PackedStringArray(["Content-Type: application/json"])
+			var game = JSON.new().stringify({
+				"game_key" : API_KEY,
+				"player_identifier": player_identifier,
+				"game_version": "0.1.0"
+			})
+			add_child(connectCurrent)
+			connectCurrent.request(
+				"https://api.lootlocker.io/game/v2/session/guest",
+				hearders,
+				HTTPClient.METHOD_POST,
+				game
+			)
+			connectCurrent.request_completed.connect(_on_request_completed)
 	else:
-		popup.set_visible(false)
-		pseudo.set_visible(true)
-		player_identifier = data["player_identifier"]
-		var hearders = PackedStringArray(["Content-Type: application/json"])
-		var game = JSON.new().stringify({
-			"game_key" : API_KEY,
-			"player_identifier": player_identifier,
-			"game_version": "0.1.0"
-		})
-		add_child(connectCurrent)
-		connectCurrent.request(
-			"https://api.lootlocker.io/game/v2/session/guest",
-			hearders,
-			HTTPClient.METHOD_POST,
-			game
-		)
-		connectCurrent.request_completed.connect(_on_request_completed)
+		var de = FileAccess.open("res://usersave/user_login.json", FileAccess.WRITE)
+		de.store_string(JSON.new().stringify({"player_identifier":null}))
+		var popup = get_node("../Panel")
+		popup.set_visible(true)
 		
-
+		
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json)

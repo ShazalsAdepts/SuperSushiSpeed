e504e5d35da467e893b0913391ccd13b8719ffd7
PIRIS Marius <marius.piris@gmail.com>
2024-01-28 14:03:06 +0100

    Fix settings

--- SuperSushiSpeed/scripts/Player.gd
+++ SuperSushiSpeed/scripts/Player.gd
@@ -86,7 +86,7 @@ func _physics_process(delta):
 	elif can_slide and Input.is_action_just_pressed("ui_shift"):
 		start_slide()
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var target_x = float(int(current_lane))
